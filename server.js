e7363c6d104e51176271286906a66dbe83d5c4d5
HaDzE715 <hadebayaa@gmail.com>
2024-01-22 08:49:38 +0200

    All changes below:

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let html = fs.readFileSync('./Templates/index.html', 'utf-8');
 let shelters = JSON.parse(fs.readFileSync('./data.json', 'utf-8')); // To read the file only once, JSON.PARSE to Convert data to javascript object notation 
 let SheltersListHTML = fs.readFileSync('./Templates/shelter-list.html', 'utf-8');
 let ReadmeHTML = fs.readFileSync('./Templates/readme.html', 'utf-8');
+
 let SheltersHTMLArr = shelters.map((shel) => {
   let output = SheltersListHTML.replace('{{%NAME%}}', shel.name);
   output = output.replace('{{%LOCATION%}}', shel.location);
@@ -13,9 +14,9 @@ let SheltersHTMLArr = shelters.map((shel) => {
 })
 
 const server = http.createServer((req, res) => { // Creatnig server using http.createServer passing request and response
-  if (req.method === 'GET' && (req.url === '/' || req.url ==='/home')) {
+  if (req.method === 'GET' && (req.url === '/' || req.url === '/home')) {
     res.writeHead(200, {
-      'Content-Type' : 'text/html',
+      'Content-Type': 'text/html',
       'my-header': 'Hello, world'
     });
     res.end(html.replace('{{%%CONTENT%%}}', ReadmeHTML));
@@ -23,35 +24,76 @@ const server = http.createServer((req, res) => { // Creatnig server using http.c
   }
   if (req.method === 'GET' && req.url === '/Style/style.css') {
     res.writeHead(200, {
-        'Content-Type': 'text/css'
+      'Content-Type': 'text/css'
     });
     const cssContent = fs.readFileSync('./Style/style.css', 'utf-8');
     res.end(cssContent);
     return;
-}
-  if (req.method === 'GET' && req.url === '/getShelters') {
+  }
+  if (req.method === 'GET' && req.url === '/Shelters') {
     let ShelterResponseHtml = html.replace('{{%%CONTENT%%}}', SheltersHTMLArr.join(''))
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.end(ShelterResponseHtml);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(ShelterResponseHtml);
+    res.end();
+    return;
   }
-  else if(req.method === 'POST' && req.url === '/addShelter'){
-    let lastId = shelters.length > 0 ? shelters[shelters.length - 1].id : 0; // Auto increment for id field
-    const newShelter = {
-      id: (parseInt(lastId) + 1),
-      name: "New Shelter",
-      location: "New Location",
-      capacity: 150
-    }
-    shelters.push(newShelter);
-    fs.writeFileSync('./data.json', JSON.stringify(shelters, null, 2));
-    console.log('New shelter added successfully.');
+  if (req.method === 'GET' && req.url === '/SheltersJSON') {
+    const SheltersJSON = fs.readFileSync('./data.json', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(SheltersJSON);
+    return;
   }
-  else if(req.method === 'PUT' && req.url.startsWith('/updateShelter')){
+  else if (req.method === 'POST' && req.url === '/CreateShelter') {
+    let requestBody = '';
+
+    req.on('data', (chunk) => {
+      requestBody += chunk;
+    });
+
+    req.on('end', () => {
+      try {
+        let requestData;
+
+        if (req.headers['content-type'] === 'application/json') // Handle requests comes from postman
+          requestData = JSON.parse(requestBody);
+
+        if (req.headers['content-type'] === 'application/x-www-form-urlencoded') // Handle requests comes from the browser
+        {
+          const querystring = require('querystring');
+          requestData = querystring.parse(requestBody);
+        }
+        const { name, location, capacity } = requestData;
+
+        let lastId = shelters.length > 0 ? shelters[shelters.length - 1].id : 0; // Auto increment for id field
+        const newShelter = {
+          id: (parseInt(lastId) + 1),
+          name: name || 'Default name',
+          location: location || "Default Location",
+          capacity: capacity || 0
+        }
+        shelters.push(newShelter);
+        fs.writeFileSync('./data.json', JSON.stringify(shelters, null, 2));
+        console.log('New shelter added successfully.');
+      } catch (error) {
+        console.error('Error parsing JSON: ', error.message);
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Shelter created successfully');
+    });
+  }
+  else if (req.method == 'GET' && req.url == '/ShelterCreation') {
+    const createShelterHTML = fs.readFileSync('./Templates/create-shelter.html', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html.replace('{{%%CONTENT%%}}', createShelterHTML));
+    return;
+  }
+  else if (req.method === 'PUT' && req.url.startsWith('/updateShelter')) {
     const shelterId = req.url.split('/')[2];
     const index = shelters.findIndex(shelter => shelter.id == shelterId);
     let updatedShelter;
 
-    if(index !== -1){
+    if (index !== -1) {
       updatedShelter = {
         id: shelterId,
         name: "Updated Shelter",
